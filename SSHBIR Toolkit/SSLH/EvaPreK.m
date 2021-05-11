cc709725b2dfcb80c42804b09a766e69366a2297
Eddie-Wang1120 <81598289+Eddie-Wang1120@users.noreply.github.com>
2021-05-11 09:32:19 +0800

    Add files via upload

---
+++ SSHBIR Toolkit/SSLH/EvaPreK.m
@@ -0,0 +1,14 @@
+ function[num,MAP] = EvaPreK(kkk,traingnd,testgnd,H,tH)
+B = compactbit(H);
+tB = compactbit(tH);
+
+hammTrainTest = hammingDist(tB, B)';
+% hash lookup: precision and reall
+% Ret = (hammTrainTest <= hammRadius+0.00001);
+% [Pre, Rec] = evaluate_macro(cateTrainTest, Ret)
+
+% hamming ranking: MAP
+[~, HammingRank]=sort(hammTrainTest,2);
+num = cat_precal(kkk,traingnd,testgnd,HammingRank');
+MAP = cat_apcal(traingnd,testgnd,HammingRank');
+ end
