f159b1e80a57eeb094bde7dcdcd41d3361331455
Eddie-Wang <81598289+Eddie-Wang1120@users.noreply.github.com>
2021-05-13 22:06:34 +0800

    Delete SSHBIR Toolkit directory

--- SSHBIR Toolkit/LFH/logExpTrick.m
+++
@@ -1,6 +0,0 @@
-% avoid floating point overflow when the values in X get too large
-function Y = logExpTrick (X)
-  Y = X;
-  dx = find(X < 100);
-  Y(dx) = log(1 + exp(X(dx)));
-end
--- SSHBIR Toolkit/RSLH/Readme.txt
+++
@@ -1,3 +0,0 @@
-Run RSLH.m to get the Precision score of RSLH-B
-
-Run Run_Fusion.m to get the Precision score of RSLH
